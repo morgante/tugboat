71a0f9197e2b375134be8a10ef0cbd5e12cb1e44
Morgante Pell <morgante.pell@morgante.net>
2014-03-12 14:41:15 +0400

    add mongoose findorcreate plugin

--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 // we need mongoose
 var mongoose = require('mongoose');
+var findOrCreate = require('mongoose-findorcreate');
+
 var _ = require('../public/lib/underscore');
 	
 var schema = mongoose.Schema({
 	"netID": String,
-	"github": String
+	"github": String,
+	"passport": String
 });
+
+// Load plugins
+schema.plugin(findOrCreate);
 
 var User = module.exports = mongoose.model('User', schema);
